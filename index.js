3ad50d340a4e88d5aab9ef837c5938b154bf0000
Bajjuri Vinay Kumar <57398305+vinaybajjuri58@users.noreply.github.com>
2020-12-23 13:03:18 +0530

    feat:added the main functionality

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var billAmount = document.querySelector("#billAmount");
-var cashGiven = document.querySelector("#cashGiven");
-var button = document.querySelector("#submitButton");
-var cashSection = document.querySelector('#cashSection')
+var billAmount = document.querySelector('#billAmount');
+var cashGiven = document.querySelector('#cashGiven');
+var button = document.querySelector('#submitButton');
+var cashSection = document.querySelector('#cashSection');
+var finalOutput = document.querySelector('#finalOutput');
 // creating function for button handler
 
 function buttonHandler(){
     var bill = billAmount.value;
-    console.log(bill)
+    var cash = cashGiven.value;
+    if(cash<bill){
+        finalOutput.innerHTML='<h1>Sorry given cash is less than bill</h1>';
+    }
+    else if(cash === bill){
+        finalOutput.innerHTML='<h1>Cash given is equal to bill</h1>';
+    }
+    else{
+        var remaining = Number(cash)-Number(bill);
+        var currency = [2000 ,500 ,100 ,20 ,10 ,5 ,1]
+    }
 }
+
 //function to show cash Given input are on when bill amount is not null
 
 function inputHandler(){
@@ -19,5 +31,7 @@ function inputHandler(){
         cashSection.hidden=false;
     }
 }
+
+
 billAmount.addEventListener('input',inputHandler);
 button.addEventListener('click',buttonHandler);
